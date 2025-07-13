b19e7a6380902e5837f3d37c93156b01dc91fba7
Vikas Bansode <131449554+Vikass19@users.noreply.github.com>
2025-07-13 21:36:07 +0530

    Update settings.py
    secrect key and db url added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lk8l82f97^6+%692je2xe@a30n=nhtkfmsy0z0!veuxvkxbbt8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-lk8l82f97^6+%692je2xe@a30n=nhtkfmsy0z0!veuxvkxbbt8')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
 
 ALLOWED_HOSTS = []
 
